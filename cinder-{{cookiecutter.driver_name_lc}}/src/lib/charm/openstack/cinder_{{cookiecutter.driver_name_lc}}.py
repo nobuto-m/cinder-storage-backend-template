d6571d8dbebc556b0cf5e08203b47e975d8a9530
Nobuto Murata <nobuto.murata@canonical.com>
2020-06-23 22:57:12 +0900

    Add two common options across storage backends

--- cinder-{{cookiecutter.driver_name_lc}}/src/lib/charm/openstack/cinder_{{cookiecutter.driver_name_lc}}.py
+++ cinder-{{cookiecutter.driver_name_lc}}/src/lib/charm/openstack/cinder_{{cookiecutter.driver_name_lc}}.py
@@ -1,6 +1,12 @@
 import charms_openstack.charm
 import charmhelpers.core.hookenv as ch_hookenv  # noqa
 
+from charmhelpers.contrib.openstack.utils import (
+    get_os_codename_package,
+    CompareOpenStackReleases,
+)
+
+
 charms_openstack.charm.use_defaults('charm.default-select-release')
 
 
@@ -16,11 +22,27 @@ class Cinder{{ cookiecutter.driver_name }}Charm(
     mandatory_config = []
 
     def cinder_configuration(self):
+        # Specify the volume driver
         volume_driver = ''
+
+        if self.config.get('volume-backend-name'):
+            volume_backend_name = self.config.get('volume-backend-name')
+        else:
+            volume_backend_name = ch_hookenv.service_name()
+
         driver_options = [
             ('volume_driver', volume_driver),
+            ('volume_backend_name', volume_backend_name),
             # Add config options that needs setting on cinder.conf
         ]
+
+        os_codename = get_os_codename_package('cinder-common')
+        if CompareOpenStackReleases(os_codename) >= "pike" \
+                and self.config.get('backend-availability-zone'):
+            driver_options.append(
+                ('backend_availability_zone',
+                 self.config.get('backend-availability-zone')))
+
         return driver_options
 
 
